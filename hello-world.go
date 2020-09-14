8b2ca5f58b4103d0606a52c9a1c9ef85c58ece4e
Tadashi Ito <10068148+itot555@users.noreply.github.com>
2020-09-14 22:53:30 +0900

    Update hello-world.go

--- hello-world.go
+++ hello-world.go
@@ -2,13 +2,12 @@ package main
 
 import (
         "net/http"
-        "fmt"
         "log"
 )
 
 func main() {
         http.HandleFunc("/", hello)
-        http.ListenAndServe(":8080", nil)
+        log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func hello(w http.ResponseWriter, r  *http.Request) {
